a279ac26f729d711802d8d4ff3771596e0538dd1
Amdersiteen <amdersiteen+github@hotmail.fr>
2023-09-25 00:38:19 +0200

    [#3 Player Node] - Final
    - Ajout animation
      - Up
      - Down
      - Left
      - Right
    - Ajout Animation state machine
    - Ajout changement de direction en jeu

--- Scene/Player/Player.gd
+++ Scene/Player/Player.gd
@@ -1,21 +1,33 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+
+@onready var animationTree : AnimationTree = $AnimationTree
+var direction : Vector2 = Vector2.ZERO
+const SPEED = 200.0
+
+func _ready():
+	direction.y = 1
+	update_animation_parameter()
+
+
 
 func _physics_process(delta):
 	
+	animationTree.active = true
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var directionH = Input.get_axis("ui_left", "ui_right")
-	var directionV = Input.get_axis("ui_up", "ui_down")
-	if directionH:
-		velocity.x = directionH * SPEED
+	direction.x  = Input.get_axis("ui_left", "ui_right")
+	direction.y  = Input.get_axis("ui_up", "ui_down")
+	if direction:
+		velocity = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if directionV:
-		velocity.y = directionV * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)	
 	
 	velocity = velocity.normalized()*SPEED
+	update_animation_parameter()
 	move_and_slide()
+
+func update_animation_parameter():
+	if(direction != Vector2.ZERO):
+		animationTree["parameters/Idle/blend_position"] = direction
