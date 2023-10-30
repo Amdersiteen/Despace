691cb24c37d4fa373411fb37bbe99e46d70b296f
Yohan MESTRE <mestre.yohan@gmail.com>
2023-10-30 13:52:40 +0100

    Explicit parameargument type

--- assets/entity/player/animate.gd
+++ assets/entity/player/animate.gd
@@ -5,7 +5,7 @@ class_name animate
 var animation_tree: AnimationTree
 
 # The constructor initializes the AnimationTree and updates the animation based on the direction.
-func _init(_animation_tree, _direction):
+func _init(_animation_tree: AnimationTree, _direction: Vector2):
 	animation_tree = _animation_tree
 	if animation_tree != null:
 		# Active l'arbre d'animation
@@ -13,11 +13,11 @@ func _init(_animation_tree, _direction):
 		update_animation(_direction)
 
 # This animate the player base on the player direction.
-func execute_animation(_direction):
+func execute_animation(_direction: Vector2):
 	if animation_tree != null:
 		update_animation(_direction)
 
 # Met à jour le paramètre d'animation en fonction du vecteur de direction.
-func update_animation(_direction):
+func update_animation(_direction: Vector2):
 	if _direction != Vector2.ZERO:
 		animation_tree["parameters/Idle/blend_position"] = _direction
--- assets/entity/player/move_player.gd
+++ assets/entity/player/move_player.gd
@@ -8,13 +8,13 @@ var direction: Vector2 = Vector2(0, 1)
 const SPEED: float = 200.0 * 60
 
 # This function return the velocity based on the player input.
-func get_velocity(delta, _velocity) -> Vector2:
+func get_velocity(delta: float, _velocity: Vector2) -> Vector2:
 	# Gère l'entrée pour le mouvement directionnel
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
 	
 	# Décélérer si aucune direction n'est fournie
-	if ! direction:
+	if direction == Vector2.ZERO:
 		_velocity.x = move_toward(_velocity.x, 0, SPEED)
 		_velocity.y = move_toward(_velocity.y, 0, SPEED)
 		return _velocity
