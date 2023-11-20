42137738c8998648df7f3f316f79ebebe5c42c41
unknown <mestre.yohan@gmail.com>
2023-11-20 14:20:01 +0100

    somme comment

--- assets/script/move player/joypad.gd
+++ assets/script/move player/joypad.gd
@@ -1,19 +1,26 @@
 extends move_strategie
 class_name joypad
 
+# Set the left joypad as the joypad that move the player
 @export var player_joypad = [JOY_AXIS_LEFT_X, JOY_AXIS_LEFT_Y]
 
+# Add the InputEventJoypadMotion in the supported event list so the manage_event methode of plqyer can chose this class
 func _init():
 	event_class_support.append("InputEventJoypadMotion")
 
 func process_input_event(_node: Node = null, event: InputEvent = null, button = false):
+	# If the joypad event is the one that can move the player
 	if event.axis in player_joypad:
+		# filter the axis value so small unwanted move of the joypad don't trigger a move or a dirrection change in the animation
 		if event.axis_value * event.axis_value > 0.22 * 0.22:
 			move_scalar[event.axis % 2] = event.axis_value
 		else:
+			# if the value is small set the corresponding axis to zero
 			move_scalar[event.axis % 2] = 0
 		
+		# Set the move variable of player to null if no move so the player can decelerate
 		if move_scalar == Vector2.ZERO:
 			_node.player.move = null
 		else:
+			# Set the player move to this so the player movement can be update itch frame
 			_node.player.move = self
